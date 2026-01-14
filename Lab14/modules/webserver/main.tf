c3d92fe26fb4478d76ad2d49ae62783f10c17dab
Rughma Malik <23-22411-054@se.fjwu.edu.pk>
2026-01-14 16:50:58 +0000

    Final submission structure for Lab 14

---
+++ Lab14/modules/webserver/main.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "web_sg" {
+  vpc_id      = var.vpc_id
+  name        = "${var.env_prefix}-web-sg-${var.instance_suffix}"
+  description = "Security group for web server allowing HTTP, HTTPS, SSH, and Gitea"
+
+  # 1. Allow SSH from anywhere 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # 2. TASK 9 REQUIREMENT: Allow Port 3000 for Gitea
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Existing HTTP rule
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Existing HTTPS rule
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "${var.env_prefix}-serverkey-${var.instance_suffix}"
+  public_key = file(var.public_key)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = "ami-05524d6658fcf35b6" # Amazon Linux 2023 Kernel 6.1 AMI
+  instance_type = var. instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  availability_zone = var.availability_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  #user_data = file(var.script_path)
+  
+  tags = {
+     Name = "${var.env_prefix}-ec2-instance-${var.instance_suffix}"
+  }
+}
